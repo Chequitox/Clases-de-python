1091d47de86ed68d46ef2ffe77b0625cc78d2795
Checo <joakingera@gmail.com>
2024-05-11 16:49:04 -0400

    hola

---
+++ variables/02oparitmetica.py
@@ -0,0 +1,13 @@
+from variables import numero1
+from variables import entero
+
+suma = numero1+entero
+
+#print(suma)
+
+numero2= input("ingresa el primero numero: ")
+numero3= float(input("ingresa el segundo numero: "))
+
+suma= numero1 + numero2 
+
+print(suma)
